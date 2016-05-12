db59aefcf75a3a08da2130825ea0ff249a1d7bb4
amazingBastard <dvazgird@beenverified.com>
2016-05-12 17:03:59 -0400

    more drag and drop logic

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -23,8 +23,9 @@ const noteTarget = {
   }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-  connectDragSource: connect.dragSource()
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging() // map isDragging() state to isDragging prop
 }))
 
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
@@ -33,11 +34,13 @@ const noteTarget = {
 
 export default class Note extends React.Component {
   render() {
-    const {connectDragSource, connectDropTarget,
-      id, onMove, ...props} = this.props;
+    const {connectDragSource, connectDropTarget, isDragging,
+      onMove, id, ...props} = this.props;
 
     return connectDragSource(connectDropTarget(
-      <li {...props}>{props.children}</li>
+      <li style={{
+        opacity: isDragging ? 0 : 1
+      }} {...props}>{props.children}</li>
     ));
   }
 }
